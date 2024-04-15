c9347f88fade3442f0a99021f5ce8a66a79bca75
Ukeme Bassey <basseyukemme@gmail.com>
2024-04-15 17:13:49 +0100

    built entire project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {useState, useEffect} from 'react';
+import iconDice from './assets/icon-dice.svg';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState({id: '', text: ''})
+  const fetchAdvice = async () => {
+    await fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then((response) => {setAdvice({id: response.slip.id, text: response.slip.advice})})
+  }
+  useEffect(() => {
+    fetchAdvice();
+  }, [])
   return (
     <div className="App">
+      <p className='advice-id'>Advice #{advice.id}</p>
+      <p className='advice-text'>❝{advice.text}❞</p>
+      <div className='pattern-divider' />
+      <div className='image-container' onClick={fetchAdvice}>
+        <img alt='icon-dice' src={iconDice} />
+      </div>
     </div>
   );
 }
